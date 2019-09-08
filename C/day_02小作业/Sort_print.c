fa4e2858feb0b719bc8f58c794699417f0197a68
Jokseia <2426733691@qq.com>
2019-09-08 22:43:14 +0800

    三个数从大到小输出

---
+++ C/day_02小作业/Sort_print.c
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//void Sort(int a, int b, int c);
+//
+//int main()
+//{
+//	int a;
+//	int b;
+//	int c;
+//	printf("please input three number:");
+//	scanf("%d%d%d",&a,&b,&c);
+//	Sort(a,b,c);
+//	system("pause");
+//	return 0;
+//}
+//void Sort(int a, int b, int c)
+//{
+//	int temp;
+//	
+//	if (a < b)
+//	{
+//		temp = b;
+//		b = a;
+//		a = temp;
+//	}
+//	if (a < c)
+//	{
+//		temp = c;
+//		c = a;
+//		a = temp;
+//	}
+//	if (b < c)
+//	{
+//		temp = c;
+//		c = b;
+//		b = temp;
+//	}
+//
+//	printf("%d %d %d", a, b, c);
+//}
