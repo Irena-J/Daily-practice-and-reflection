8b46e3cb30142851abc03cd8745d381203dbe0f6
Jokseia <2426733691@qq.com>
2019-09-08 22:37:20 +0800

    c语言第二次小作业

---
+++ C/day_02小作业/change1.c
@@ -0,0 +1,26 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//void Swap2(int a, int b);
+//
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	Swap2(a, b);
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void Swap2(int a, int b)
+//{
+//	b = b - a;
+//	a = a + b;
+//	b = a - b;
+//
+//
+//	printf("%d %d\n", a, b);
+//}
