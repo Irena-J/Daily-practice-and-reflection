43abc84dcac1a26b741586828596aa53e6933ec8
Jokseia <2426733691@qq.com>
2019-09-13 23:21:43 +0800

    练习代码

---
+++ Guess_number.c
@@ -0,0 +1,38 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+//int main()
+//
+//{
+//	srand((unsigned)time(NULL));
+//	int a = rand()%100+1;
+//	int b;
+//	
+//	while (1)
+//	{
+//		printf("please input a number(0-100):");
+//		scanf("%d", &b);
+//		if (b > a)
+//		{
+//			printf("你猜的数字大了");
+//			
+//			
+//		}
+//		else if (b < a)
+//
+//			printf("你猜的数字小了");
+//		
+//		else if (b = a)
+//		{
+//			printf("恭喜你，猜对了");
+//			break;
+//		}
+//
+//	}
+//	system("pause");
+//	return 0;
+//}
