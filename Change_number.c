4ebd0d0dc726c3fef9d6af7490aac679ac0cbebc
Jokseia <2426733691@qq.com>
2019-09-13 23:19:47 +0800

    小作业

---
+++ Change_number.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//
+////void Change_num(int *a, int *b);
+//
+//void Change_num(int *a, int *b)
+//{
+//	int temp = 0;
+//	temp = *a;
+//	a = *b;
+//	b = temp;
+//	printf("%d %d",a, b);
+//}
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	Change_num(&a, &b);
+//	system("pause");
+//	return 0;
+//}
