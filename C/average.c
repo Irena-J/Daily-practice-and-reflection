780afe802419f592ef0a033033973f2ea9494f2d
Jokseia <2426733691@qq.com>
2019-09-23 23:36:01 +0800

    位运算求平均值

---
+++ C/average.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+int Average_Num1(int a, int b)
+{
+	return (a + b) >> 1;
+}
+int Average_Num2(int a, int b)
+{
+	return (a & b)+((a^b) >> 1);
+}
+int Average_Num3(int a, int b)
+{
+	return a+((b-a)>>1);
+}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	printf("please input two number:");
+	scanf("%d%d",&a,&b);
+	printf("%d\n",Average_Num1(a, b));
+	printf("%d\n", Average_Num2(a, b));
+	printf("%d\n", Average_Num3(a, b));
+	system("pause");
+	return 0;
+}
