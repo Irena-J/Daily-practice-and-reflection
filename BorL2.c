2b570aae69de24dff3aa9a63b1d0bb5f249391ab
Jokseia <2426733691@qq.com>
2019-09-28 21:43:43 +0800

    第二种方法判断大小端

---
+++ BorL2.c
@@ -0,0 +1,18 @@
+//#define _crt_secure_no_warnings 1
+//#include<stdio.h>
+//#include<windows.h>
+
+//union un
+//{
+//	int a;
+//	char ch;
+//};
+//int main()
+//{
+//	union un uu;
+//	uu.a = 0x11223344;
+//	rand() % 12;
+//	printf("%x\n", uu.ch);
+//	system("pause");
+//	return 0;
+//}
