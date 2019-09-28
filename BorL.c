879d78f4bd67ee6c46489ad2fd238227075efe69
Jokseia <2426733691@qq.com>
2019-09-28 21:42:42 +0800

    大小端问题

---
+++ BorL.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<windows.h>
+//
+////十进制
+//int main()
+//{
+//	int a = 1535778833;
+//	char*p = (char*)&a;
+//	printf("%d", *p);
+//	system("pause");
+//	return 0;
+//}
+//
+////十六进制
+//
+//int main()
+//{
+//	int a = 0x11223344;
+//	char*pc = (char*)&a;
+//	printf("%x", *pc);
+//	system("pause");
+//	return 0;
+//}
