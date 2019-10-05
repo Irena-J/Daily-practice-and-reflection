5ce5bab086a9dfbc6edacb2a6a1b63a6d46ad022
Jokseia <2426733691@qq.com>
2019-10-05 23:31:02 +0800

    关于sizeof()

---
+++ C/change.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));
+}
+
+
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = {0};
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(ch));
+	test1(arr);
+	test2(ch);
+	system("pause");
+	return 0;
+}
