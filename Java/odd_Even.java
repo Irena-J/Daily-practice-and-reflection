fcceb7a5a7d8a961d8ab2a55519b761d24f405d6
Jokseia <2426733691@qq.com>
2019-10-12 23:57:55 +0800

    移位运算的用法

---
+++ Java/odd_Even.java
@@ -0,0 +1,12 @@
+public class odd_Even{
+	public static void main(String[] args){
+		for(int i=30;i>=0;i-=2){
+			int odd=(num>>i)&1;
+			System.out.println(odd);
+		}
+		for(int i=31;i>0;i-=2){
+			int even =(num>>i)&1;
+			System.out.println(enen);
+		}
+	}
+}
