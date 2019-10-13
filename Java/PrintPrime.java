f945663f37022458aca0d0c3b6013a5e84b3290a
Jokseia <2426733691@qq.com>
2019-10-13 22:35:06 +0800

    设置中间量

---
+++ Java/PrintPrime.java
@@ -0,0 +1,17 @@
+public class PrintPrime{
+	public static void main(String[] args){
+		for(int i=2;i<=100;i++){
+			boolean flag = true;
+			for(int j=2;j<i;j++){
+				if(i%j==0){
+					flag=false;
+					break;
+				}
+			}
+			if(flag){
+			System.out.println(i);
+			}
+		}
+	
+	}
+}
