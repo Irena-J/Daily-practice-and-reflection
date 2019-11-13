08acf7ca114ada6970f40e809d0d7ae605580ceb
Jokseia <2426733691@qq.com>
2019-11-13 22:42:19 +0800

    是否有环

---
+++ OJ/hasCycle.java
@@ -0,0 +1,16 @@
+   public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                break;
+            }
+        }
+        if(fast == null || fast.next == null){
+            return false;
+        }
+        return true;
+    
