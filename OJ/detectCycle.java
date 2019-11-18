6615d54a69ff43c2ae6e2ba131c20f1a37e6fd5f
Jokseia <2426733691@qq.com>
2019-11-18 22:58:58 +0800

    环形链表的入口节点

---
+++ OJ/detectCycle.java
@@ -0,0 +1,23 @@
+public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                break;
+            }
+        }
+        slow = head;
+        while(fast != null && fast != slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        if(fast == slow){
+            return slow;
+        }
+        return null;
+    }
