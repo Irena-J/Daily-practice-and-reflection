a0a232b292998f8bd6d5de2388d86784417423c2
Jokseia <2426733691@qq.com>
2019-12-19 19:27:09 +0800

    存储自定义对象的遍历

---
+++ Java/StudentDemo.java
@@ -0,0 +1,33 @@
+class Student{
+		private String name;
+		private int age;
+		
+		public Student(){}
+		publc Student(String name,int age){
+			this.name = name;
+			this.age = age;
+		}
+		public String getName(){
+			return this.name;
+		}
+		public int age(){
+			return thia.age;
+		}
+	}
+	public class StudentDemo{
+		publlic static void main(String[] args){
+			Collection c = new ArrayList();
+			Student s1 = new Student("lily",3);
+			Student s2 = new Student("Jokesia",18);
+			Student s3 = new Student("hebi",22);
+			
+			c.add(s1);
+			c.add(s2);
+			c.add(s3);
+			//由于作用在局部，用完就回收了，效率比较高
+			for(Iterator it = c.iterator();it.hasNext();){
+				Student s = (Student) it.next();
+				System.out.println(s);
+			}
+		}
+	}
