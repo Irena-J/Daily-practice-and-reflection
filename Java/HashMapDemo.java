b7183e415e101dbb8a3371c3ef03c1c0f59fbc15
Jokseia <2426733691@qq.com>
2020-01-18 21:53:42 +0800

    键为自定义对象

--- Java/HashMapDemo.java
+++ Java/HashMapDemo.java
@@ -89,3 +89,58 @@ HashMap<Integer,String>
 		}
 	}
 	
+	
+HashMap<Student,String>
+
+键：Student
+	要求：如果两个对象的成员变量值都相同,则为同一个对象
+值：String
+
+	public class HashMapDemo4 {
+		public static void main(String[] args) {
+			//创建集合对象
+			HashMap<Student,String> hm = new HashMap<Student,String>();
+			
+			//创建学生对象
+			Student s1 = new Student("leyan",18);
+			Student s2 = new Student("the shy",19);
+			Student s3 = new Student("rookie",21);
+			Student s4 = new Student("the shy",19);
+			
+			//添加元素
+			hm.put(s1,"202001");
+			hm.put(s2,"202002");
+			hm.put(s3,"202003");
+			hm.put(s4,"202004");
+			
+			//遍历
+			Set<Student> set = hm.keySet();
+			for(Student key : set) {
+				String value = hm.get(key);
+				//根据要求为了防止s4的重复要在Student中重写hashCode和equals方法
+				//键相同值覆盖
+				System.out.println(key.getName()+"———"+key.getAge+"--"+value);
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
