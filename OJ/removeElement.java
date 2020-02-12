e9a30b2eea281fae5d47a035dde7b92cc8ae6f6a
Jokseia <2426733691@qq.com>
2020-02-12 20:58:05 +0800

    双指针法移除元素

--- OJ/removeElement.java
+++ OJ/removeElement.java
@@ -10,3 +10,18 @@ class Solution {
         return count;
     }
 }
+
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+}
+
