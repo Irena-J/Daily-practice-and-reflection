0ce61772e523da0a3dfca9f2e548526513a2dcfc
Jokseia <2426733691@qq.com>
2020-02-18 23:18:29 +0800

    登录类

---
+++ VerifyEqual.java
@@ -0,0 +1,22 @@
+package cn.itDemo;
+
+import java.util.Scanner;
+
+/**
+ * 验证管理员登录 
+ */
+public class VerifyEqual {
+
+	public VerifyEqual() {
+	}
+
+	public boolean verify(String s, String s1) {
+		System.out.print("请输入用户名：");
+		Scanner scanner = new Scanner(System.in);
+		String s2 = scanner.next();
+		System.out.print("请输入密码：");
+		scanner = new Scanner(System.in);
+		String s3 = scanner.next();
+		return s2.equals(s) && s1.equals(s3);
+	}
+}
