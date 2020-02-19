2beb09de10ef51651554dfe3f20ade6fbf93da87
Jokseia <2426733691@qq.com>
2020-02-19 23:42:36 +0800

    manager

---
+++ Manager.java
@@ -0,0 +1,15 @@
+package cn.itDemo;
+
+/**
+ 
+ */
+public class Manager {
+
+	public Manager() {
+		username = "itcast";
+		password = "itcast";
+	}
+
+	public String username;
+	public String password;
+}
