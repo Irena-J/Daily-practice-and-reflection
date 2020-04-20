1d660def027b5c9b77111e84a07f8733103f5b4d
Jokseia <2426733691@qq.com>
2020-04-20 23:08:30 +0800

    判断字串

---
+++ algorithm/chkSubStr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Substr {
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        boolean[] arr = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.contains(p[i]);
+        }
+        return arr;
+    }
+}
